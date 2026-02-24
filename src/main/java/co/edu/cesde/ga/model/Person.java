d689b22f477d2bebce0baeb7de599c21958ed361
Luis Goenaga <lgoenaga@cesde.net>
2026-02-24 07:49:19 -0500

    Creación de model person

---
+++ src/main/java/co/edu/cesde/ga/model/Person.java
@@ -0,0 +1,12 @@
+package co.edu.cesde.ga.model;
+
+public class Person {
+
+    Long userId;
+    String code;
+    String documentNumber;
+    String firstName;
+    String lastName;
+    String status;
+}
+
