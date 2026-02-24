9eea4ef66a87434ad3425746d5106b6804b40c27
Luis Goenaga <lgoenaga@cesde.net>
2026-02-24 10:00:58 -0500

    Herencia de person, para las clases student y teacher, impresión en el main

--- src/main/java/co/edu/cesde/ga/app/Main.java
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -1,6 +1,8 @@
 package co.edu.cesde.ga.app;
 
 import co.edu.cesde.ga.model.Person;
+import co.edu.cesde.ga.model.Student;
+import co.edu.cesde.ga.model.Teacher;
 
 public class Main {
 
@@ -30,5 +32,62 @@ public class Main {
         System.out.println("First Name: " + person2.getFirstName());
         System.out.println("Last Name: " + person2.getLastName());
         System.out.println("Status: " + person2.getStatus());
+
+        Teacher teacher = new Teacher();
+
+        teacher.setUserId(3L);
+        teacher.setCode("789012");
+        teacher.setDocumentNumber("1122334455");
+        teacher.setFirstName("Alice");
+        teacher.setLastName("Johnson");
+        teacher.setStatus("ACTIVE");
+
+        System.out.println("User ID: " + teacher.getUserId());
+        System.out.println("Code: " + teacher.getCode());
+        System.out.println("Document Number: " + teacher.getDocumentNumber());
+        System.out.println("First Name: " + teacher.getFirstName());
+        System.out.println("Last Name: " + teacher.getLastName());
+        System.out.println("Status: " + teacher.getStatus());
+
+        Teacher teacher2 = new Teacher(4L, "210987", "5544332211", "Bob", "Williams", "INACTIVE");
+
+        System.out.println("User ID: " + teacher2.getUserId());
+        System.out.println("Code: " + teacher2.getCode());
+        System.out.println("Document Number: " + teacher2.getDocumentNumber());
+        System.out.println("First Name: " + teacher2.getFirstName());
+        System.out.println("Last Name: " + teacher2.getLastName());
+        System.out.println("Status: " + teacher2.getStatus());
+
+        Student student = new Student();
+
+        student.setUserId(5L);
+        student.setCode("345678");
+        student.setDocumentNumber("6677889900");
+        student.setFirstName("Charlie");
+        student.setLastName("Brown");
+        student.setStatus("ACTIVE");
+        student.setBirthDate("2000-01-01");
+
+        System.out.println("User ID: " + student.getUserId());
+        System.out.println("Code: " + student.getCode());
+        System.out.println("Document Number: " + student.getDocumentNumber());
+        System.out.println("First Name: " + student.getFirstName());
+        System.out.println("Last Name: " + student.getLastName());
+        System.out.println("Status: " + student.getStatus());
+        System.out.println("Birth Date: " + student.getBirthDate());
+
+        Student student2 = new Student(6L, "876543", "0099887766", "David", "Smith", "INACTIVE", "1999-12-31");
+
+        System.out.println("User ID: " + student2.getUserId());
+        System.out.println("Code: " + student2.getCode());
+        System.out.println("Document Number: " + student2.getDocumentNumber());
+        System.out.println("First Name: " + student2.getFirstName());
+        System.out.println("Last Name: " + student2.getLastName());
+        System.out.println("Status: " + student2.getStatus());
+        System.out.println("Birth Date: " + student2.getBirthDate());
+
+
+
+
     }
 }
---
+++ src/main/java/co/edu/cesde/ga/model/Teacher.java
@@ -0,0 +1,11 @@
+package co.edu.cesde.ga.model;
+
+public class Teacher extends  Person {
+    public Teacher() {
+
+    }
+
+    public Teacher(Long userId, String code, String documentNumber, String firstName, String lastName, String status) {
+        super(userId, code, documentNumber, firstName, lastName, status);
+    }
+}
